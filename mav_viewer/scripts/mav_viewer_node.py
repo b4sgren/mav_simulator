b012a987bcc3c5c7b60b9480729d79844407d1a9
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-21 16:50:44 -0600

    Ready to test this viewer

--- mav_viewer/scripts/mav_viewer_node.py
+++ mav_viewer/scripts/mav_viewer_node.py
@@ -4,3 +4,30 @@ from dynamics.msg import State
 
 from mav_viewer import MAV_Viewer as MAV
 import numpy as np
+
+class Viewer:
+    def __init__(self):
+        self.mav_viewer = MAV()
+        self.pose_sub = rospy.Subscriber('true_state', State, self.state_callback, queue_size=1)
+
+    def state_callback(self, msg):
+        state = np.array([msg.pn, msg.pe, msg.h, msg.phi, msg.theta, msg.psi])
+        self.mav_viewer.update(state)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            rospy.spin()
+
+if __name__ == '__main__':
+    """
+    This is the visualization of the simulator
+    """
+
+    print "Initializing node"
+    rospy.init_node("simulator", log_level=rospy.DEBUG) #use when running ros
+
+    #Initialize class here:
+    viewer = Viewer()
+    viewer.run() # will stay commented out while building rest of class
+
+print "Done"
