03228f1f2c2c88f615ab19e1a629f14cf6e6c208
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-09 20:49:53 -0600

    I believe the header file is mostly done

--- dynamics/include/dynamics/wind_sim.h
+++ dynamics/include/dynamics/wind_sim.h
@@ -10,8 +10,24 @@ using namespace dyn
   class WindSim
   {
   public:
+    WindSim();
+    ~WindSim();
 
   private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_p;
+
+    ros::Subscriber state_sub;
+    ros::Publisher wind_pub;
+
+    void timerCallback(const ros::TimerEvent& event);
+
+    Eigen::Matrix<double, 5, 3> B;
+    Eigen::Vector3d wind_ss;
+    Eigen::Vector3d wind_gust;
+    double Va_;
+    //Dryden Gust model Params
+    double Lu, Lv, LW, sigma_u, sigma_v, sigma_w;
   };
 }
 
