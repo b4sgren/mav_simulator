8f8a0585dff84c35c8db8f2e382acc37272c07cd
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-11 13:28:44 -0600

    pid controller class is written and it builds. It is ros independent. Now to do the autopilot class

--- autopilot/include/autopilot/pid_controller.h
+++ autopilot/include/autopilot/pid_controller.h
@@ -2,15 +2,27 @@
 #define PID
 
 #include <ros/ros.h>
-#include <Eigen/Core>
 
 namespace control
 {
   class PID_Controller
   {
   public:
+    PID_Controller(double kp, double kd, double ki, double sigma, double sat_l, double sat_h);
+    ~PID_Controller();
 
   private:
+    double update(double yref, double y, double dt, bool wrap_flag);
+    double updateWithRate(double yref, double y, double ydot, double dt);
+    void integrateError(double error, double dt);
+    void differentiate(double y, double dt);
+    void antiWindUp(double u_unsat, double u, double dt);
+    double saturate(double u);
+
+    double kp_, kd_, ki_, sigma_, limit_h_, limit_l_;
+    double integrator_, eprev_, ydot_, yprev_;
+    const double PI{3.14159625};
+
   };
 }
 #endif
--- autopilot/src/pid_controller.cpp
+++ autopilot/src/pid_controller.cpp
@@ -2,5 +2,86 @@
 
 namespace control
 {
-  
+  PID_Controller::PID_Controller(double kp, double kd, double ki, double sigma, double sat_l, double sat_h)
+  {
+    kp_ = kp;
+    kd_ = kd;
+    ki_ = ki;
+    sigma_= sigma;
+    limit_h_ = sat_h;
+    limit_l_ = sat_l;
+    integrator_ = 0.0;
+    eprev_ = 0.0;
+    yprev_ = 0.0;
+    ydot_ = 0.0;
+  }
+
+  PID_Controller::~PID_Controller(){}
+
+  double PID_Controller::update(double yref, double y, double dt, bool wrap_flag)
+  {
+    double error{yref - y};
+    if(wrap_flag)
+    {
+      while(error > PI)
+        error -= 2 * PI;
+      while(error <= -PI)
+        error += 2 * PI;
+    }
+    integrateError(error, dt);
+    differentiate(y, dt);
+
+    double u_unsat{kp_ * error + ki_ * integrator_ - kd_ * ydot_};
+    double u_sat{saturate(u_unsat)};
+
+    if(!ki_ == 0)
+      antiWindUp(u_unsat, u_sat, dt);
+
+    return u_sat;
+  }
+
+  double PID_Controller::updateWithRate(double yref, double y, double ydot, double dt)
+  {
+    double error{yref - y};
+    integrateError(error, dt);
+
+    double u_unsat{kp_ * error + ki_ * integrator_ - kd_ * ydot};
+    double u_sat{saturate(u_unsat)};
+
+    if(!ki_ == 0)
+      antiWindUp(u_unsat, u_sat, dt);
+
+    return u_sat;
+  }
+
+  void PID_Controller::integrateError(double error, double dt)
+  {
+    integrator_ += dt/2.0 * (error + eprev_);
+    eprev_ = error;
+  }
+
+  void PID_Controller::differentiate(double y, double dt)
+  {
+    double a1 = (2.0 * sigma_ - dt) / (2.0 * sigma_ + dt);
+    double a2 = 2.0 / (2.0 * sigma_ + dt);
+
+    ydot_ = a1 * ydot_ + a2 * (y - yprev_);
+  }
+
+  void PID_Controller::antiWindUp(double u_unsat, double u, double dt)
+  {
+    integrator_ += dt/ki_ * (u - u_unsat);
+  }
+
+  double PID_Controller::saturate(double u)
+  {
+    double usat;
+    if(u >= limit_h_)
+      usat = limit_h_;
+    else if(u <= limit_l_)
+      usat = limit_l_;
+    else
+      usat = u;
+    return usat;
+  }
 }
