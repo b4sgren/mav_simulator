b1a19cb1b8b0a224d6b37d250376916e2b810d44
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-13 11:59:23 -0600

    Added the final launch file. Now to go debug autopilot

--- autopilot/src/autopilot.cpp
+++ autopilot/src/autopilot.cpp
@@ -7,12 +7,12 @@ namespace control
   {
     //Write a parameter file
     double roll_kp, roll_kd, course_kp, course_ki, kp_yaw, tau_yaw;
-    nh_p.param<double>("roll_kp", roll_kp, 0.0);
-    nh_p.param<double>("roll_kd", roll_kd, 0.0);
-    nh_p.param<double>("course_kp", course_kp, 0.0);
-    nh_p.param<double>("course_ki", course_ki, 0.0);
-    nh_p.param<double>("yaw_damper_tau_r", tau_yaw, 0.0);
-    nh_p.param<double>("yaw_damper_kp", kp_yaw, 0.0);
+    nh_.param<double>("roll_kp", roll_kp, 0.0);
+    nh_.param<double>("roll_kd", roll_kd, 0.0);
+    nh_.param<double>("course_kp", course_kp, 0.0);
+    nh_.param<double>("course_ki", course_ki, 0.0);
+    nh_.param<double>("yaw_damper_tau_r", tau_yaw, 0.0);
+    nh_.param<double>("yaw_damper_kp", kp_yaw, 0.0);
     roll_from_aileron = PID_Controller(roll_kp, roll_kd, 0.0, radians(45), -radians(45));
     course_from_roll = PID_Controller(course_kp, 0, course_ki, radians(30), -radians(30));
     //Yaw damper: Note that these values are specific to the values given in the control_params.yaml file
@@ -23,12 +23,12 @@ namespace control
     x_ = 0.0;
 
     double pitch_kp, pitch_kd, alt_kp, alt_ki, throttle_kp, throttle_ki;
-    nh_p.param<double>("pitch_kp", pitch_kp, 0.0);
-    nh_p.param<double>("pitch_kd", pitch_kd, 0.0);
-    nh_p.param<double>("altitude_kp", alt_kp, 0.0);
-    nh_p.param<double>("altitude_ki", alt_ki, 0.0);
-    nh_p.param<double>("throttle_kp", throttle_kp, 0.0);
-    nh_p.param<double>("throttle_ki", throttle_ki, 0.0);
+    nh_.param<double>("pitch_kp", pitch_kp, 0.0);
+    nh_.param<double>("pitch_kd", pitch_kd, 0.0);
+    nh_.param<double>("altitude_kp", alt_kp, 0.0);
+    nh_.param<double>("altitude_ki", alt_ki, 0.0);
+    nh_.param<double>("throttle_kp", throttle_kp, 0.0);
+    nh_.param<double>("throttle_ki", throttle_ki, 0.0);
     pitch_from_elevator = PID_Controller(pitch_kp, pitch_kd, 0, radians(45), -radians(45));
     altitude_from_pitch = PID_Controller(alt_kp, 0, alt_ki, radians(30), -radians(30));
     airspeed_from_throttle = PID_Controller(throttle_kp, 0, throttle_ki, 1.0, 0.0);
