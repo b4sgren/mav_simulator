2bd626f95811c903d4b5efd2175efab5e821bc22
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-16 10:48:51 -0600

    Did a little bit of clean up

--- mav_viewer/scripts/mav_viewer_node.py
+++ mav_viewer/scripts/mav_viewer_node.py
@@ -4,17 +4,13 @@ from dynamics.msg import State
 
 from mav_viewer import MAV_Viewer as MAV
 import numpy as np
-# import pyqtgraph.opengl as gl
 import pyqtgraph as pg
-
-from threading import Thread
 
 class Viewer:
     def __init__(self):
         self.mav_viewer = MAV()
-        rospy.init_node("simulator", log_level=rospy.DEBUG) #initialize at end of constructor
+        rospy.init_node("simulator", log_level=rospy.DEBUG)
         self.pose_sub = rospy.Subscriber('true_states', State, self.state_callback, queue_size=1)
-        pg.QtGui.QApplication.instance().exec_()
 
     def state_callback(self, msg):
         state = np.array([msg.pn, msg.pe, msg.h, msg.phi, msg.theta, msg.psi])
@@ -28,13 +24,7 @@ if __name__ == '__main__':
     """
     This is the visualization of the simulator
     """
-
-    #set this so that mav_viewer is main window. Create separate threads inside of it to run other ros nodes.
-    print "Initializing node"
-
     #Initialize class here:
     viewer = Viewer()
-    thread = Thread(target=viewer.run()) # try running this in a separate thread
-    thread.start()
-
-print "Done"
+    pg.QtGui.QApplication.instance().exec_()
+    # viewer.run() # not needed. The previous line performs the same function
